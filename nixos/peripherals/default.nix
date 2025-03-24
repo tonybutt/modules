12abad18089260ff111ecc0ab3162b3b1e632f88
Anthony Butt <anthony.butt@secondfront.com>
2025-03-24 12:50:18 -0400

    fix(nixos): fixup module errors
    Signed-off-by: Anthony Butt <anthony.butt@secondfront.com>

--- nixos/peripherals/default.nix
+++ nixos/peripherals/default.nix
@@ -15,8 +15,7 @@ in
       kernelModules = mkIf cfg.obs.enable [ "v4l2loopback" ] ++ (if cfg.scarlettRite.enable then [ "snd_aloop" ] else []);
       extraModulePackages = mkIf cfg.obs.enable [ config.boot.kernelPackages.v4l2loopback.out ];
       extraModprobeConfig =
-        config.boot.extraModprobeConfig
-        + (if cfg.obs.enable then "options v4l2loopback exclusive_caps=1 card_label='OBS Camera'" else "")
+        (if cfg.obs.enable then "options v4l2loopback exclusive_caps=1 card_label='OBS Camera'" else "")
         + (
           if cfg.scarlettRite.enable then "options snd_usb_audio vid=0x1235 pid=0x8212 device_setup=1" else ""
         );
