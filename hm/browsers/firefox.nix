ec986149cfb48a5c1d105c7a6544baf074c14527
Anthony Butt <anthony.butt@secondfront.com>
2025-04-19 00:11:44 -0400

    fix: remove extension settings from modules
    Signed-off-by: Anthony Butt <anthony.butt@secondfront.com>

--- hm/browsers/firefox.nix
+++ hm/browsers/firefox.nix
@@ -63,29 +63,6 @@ in
           DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
           SearchBar = "unified"; # alternative: "separate"
 
-          # ---- EXTENSIONS ----
-          # Check about:support for extension/add-on ID strings.
-          # Valid strings for installation_mode are "allowed", "blocked",
-          # "force_installed" and "normal_installed".
-          ExtensionSettings = {
-            "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-            # uBlock Origin:
-            "uBlock0@raymondhill.net" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-              installation_mode = "force_installed";
-            };
-            # Privacy Badger:
-            "jid1-MnnxcxisBPnSXQ@jetpack" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-              installation_mode = "force_installed";
-            };
-            # Bitwarden
-            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/file/4387077/bitwarden_password_manager-2024.11.0.xpi";
-              installation_mode = "force_installed";
-            };
-          };
-
           # ---- PREFERENCES ----
           # Check about:config for options.
           Preferences = {
