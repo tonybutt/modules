18c4d8586e54d3e4929741d18c2c82832b54247b
Eric Wardner <ewardner22@gmail.com>
2025-08-03 14:50:45 -0400

    feat: add link to source of equalizer settings

--- nixos/peripherals/dellXpsAudioEqualizer.nix
+++ nixos/peripherals/dellXpsAudioEqualizer.nix
@@ -11,6 +11,7 @@ let
 in
 {
   config = mkIf cfg.dellXpsEqualizer.enable {
+    # https://gist.github.com/alexVinarskis/77d55a0a0f4150576ba77e5f4241d512
     services.pipewire = {
       enable = true;
       pulse.enable = true;
